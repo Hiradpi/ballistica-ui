54bda3532d91ded0258da0dd3ad8c0c1fd531361
Hiradpi <78161225+Hiradpi@users.noreply.github.com>
2023-08-03 04:41:22 +0330

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
-const IP = "12.45.234.234";
+const IP = "107.189.29.188";
 const PORT = "43210";
 // change above IP PORT ONLY 
 async function handler(req: Request): Promise<Response> {
